01f1466ae8f92e78621fd33eaa71f10005477a0b
oriversegura <oriverseguravargas@gmail.com>
2026-04-16 11:03:48 -0400

    add telegram alerts

--- internal/telegram/notify.go
+++ internal/telegram/notify.go
@@ -1,11 +1,37 @@
 package telegram
 
-type Message struct {
-	chat_id string
-	text    string
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type message struct {
+	Chat_id string `json:"chat_id"`
+	Text    string `json:"text"`
 }
 
-func SendAlert(chatID string, token string, message string) (error error) {
+func SendAlert(chatID string, token string, msg string) (error error) {
+
+	body := message{
+		Chat_id: chatID,
+		Text:    msg,
+	}
+
+	jsonBody, err := json.Marshal(body)
+	if err != nil {
+		return err
+	}
+
+	url := fmt.Sprintf("https://api.telegram.org/bot%s/sedMessage", token)
+
+	res, err := http.Post(url, "application/json", bytes.NewBuffer(jsonBody))
+	if err != nil {
+		return err
+	}
+
+	defer res.Body.Close()
 
 	return nil
 }
